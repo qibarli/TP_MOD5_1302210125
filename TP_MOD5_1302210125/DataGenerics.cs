1aa2062b8344d609a4434f34b81fa826513c5ef8
qibarli <barliant.rifqi20@gmail.com>
2023-03-20 00:45:58 +0700

    menambahkan class DataGeneric

---
+++ TP_MOD5_1302210125/DataGenerics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD5_1302210125
+{
+    internal class DataGenerics<T>
+    {
+        private T data;
+
+        public DataGenerics(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data Yang Tersimpan: " + this.data);
+        }
+    }
+}
--- TP_MOD5_1302210125/Program.cs
+++ TP_MOD5_1302210125/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TP_MOD5_1302210125;
 using TP_MOD5_130221205;
 
 namespace TP_MOD5_KPL_1302210125
@@ -9,6 +10,8 @@ namespace TP_MOD5_KPL_1302210125
         {
             HelloGenerics HG = new HelloGenerics();
             HG.SapaUSer<String>("BAR");
+            DataGenerics<String> data_saved = new DataGenerics<string>("1302210125");
+            data_saved.PrintData();
         }
     }
 }
